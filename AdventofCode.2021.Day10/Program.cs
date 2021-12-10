22ff44e0a1aa0400b4cd470ff6ab543a8352bebf
Casino Corley <ccorley@healthgrades.com>
2021-12-10 00:27:46 -0700

    Day 10

--- AdventofCode.2021.Day10/Program.cs
+++ AdventofCode.2021.Day10/Program.cs
@@ -1,13 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("******* Part 1 ********");
-AnswerPart1("ExampleData.txt");
+AnswerPart1("ExampleData1.txt");
 AnswerPart1("PuzzleData.txt");
 
 Console.WriteLine();
 
 Console.WriteLine("******* Part 2 ********");
-AnswerPart2("ExampleData.txt");
+AnswerPart2("ExampleData2.txt");
 AnswerPart2("PuzzleData.txt");
 
 void AnswerPart1(string file)
@@ -21,7 +21,7 @@ void AnswerPart2(string file)
 {
     var data = GetData(file);
     var answer = GetAnswerPart2(data);
-    Console.WriteLine($"{file}: {answer}");
+    Console.WriteLine($"{file}: {answer.ToString()}");
 }
 
 List<string> GetData(string fileName)
@@ -37,112 +37,177 @@ int GetAnswerPart1(List<string> data)
     if (!data.Any())
         return 0;
 
+    var stack = new Stack<char>();
+    
+    char letter = default;
+    var isBad = false;
 
-    foreach (var letter in data[0])
+    var answer = 0;
+
+    foreach (var line in data)
     {
-        
-    }
-    
-    
-    
-    var parenthese = new List<int>();
-    var square = new List<int>();
-    var bracket = new List<int>();
-    var caret = new List<int>();
-
-    char orphan = default;
-
-    for (var i = 0; i < data[0].Length; i++)
-    {
-        var letter = data[0][i];
-        
-        if (letter == '(')
-            parenthese.Add(i);
-        else if (letter == ')')
+        for (var i = 0; i < line.Length; i++)
         {
-            if (parenthese.Count == 0)
+            letter = line[i];
+            isBad = false;
+            
+            switch (letter)
             {
-                orphan = letter;
-                break;
-            }
-            parenthese.RemoveLast();
-
-        }
-        else if (letter == '[')
-            square.Add(i);
-        else if (letter == ']')
-        {
-            if (square.Count == 0)
-            {
-                orphan = letter;
-                break; 
-            }
-            square.RemoveLast();
-
-        }
-        else if (letter == '{')
-            bracket.Add(i);
-        else if (letter == '}')
-        {
-            if (bracket.Count == 0)
-            {
-                orphan = letter;
-                break; 
-            }
-            bracket.RemoveLast();
-
-        }
-        else if (letter == '<')
-            caret.Add(i);
-        else if (letter == '>')
-        {
-            if (caret.Count < 0)
-            {
-                orphan = letter;
-                break; 
-            }
-            caret.RemoveLast();
-        }
-
-        if (orphan != default)
-        {
-            switch (orphan)
-            {
+                case '(':
+                    stack.Push(letter);
+                    break;
                 case ')':
-                    return 3;
+                    isBad = stack.SafePop() != '(';
+                    break;
+            
+                case '[':
+                    stack.Push(letter);
+                    break;
                 case ']':
-                    return 57;
+                    isBad = stack.SafePop() != '[';
+                    break;
+            
+                case '{':
+                    stack.Push(letter);
+                    break;
                 case '}':
-                    return 1197;
+                    isBad = stack.SafePop() != '{';
+                    break;
+            
+                case '<':
+                    stack.Push(letter);
+                    break;
                 case '>':
-                    return 25137;
+                    isBad = stack.SafePop() != '<';
+                    break;
+            
                 default:
-                    return 0;
+                    break;
             }
-        }
 
-        int? p = parenthese.Any() ? parenthese.Min() : null;
-        int? s = square.Any() ? square.Min() : null;
-        int? b = bracket.Any() ?  bracket.Min() : null;
-        int? c = caret.Any() ? caret.Min() : null;
+            if (isBad)
+                break;
+        }
+        
+        if (!isBad) continue;
+    
+        switch (letter)
+        {
+            case ')': 
+                answer += 3;
+                break;
+            case ']': 
+                answer += 57;
+                break;
+            case '}': 
+                answer += 1197;
+                break;
+            case '>': 
+                answer += 25137;
+                break;
+        }
     }
     
-    return 0;
+    return answer;
 }
 
-
-string GetAnswerPart2(List<string> data)
+long GetAnswerPart2(List<string> data)
 {
-    return "";
+    if (!data.Any())
+        return 0;
+
+    var answers = new List<long>();
+    
+    var pointsMap = new Dictionary<char, long>
+    {
+        {'(', 1},
+        {'[', 2},
+        {'{', 3},
+        {'<', 4}
+    };
+
+    foreach (var line in data)
+    {
+        var stack = new Stack<char>();
+        char letter = default;
+        var isCorrupted = false;
+        long answer = 0;
+
+        for (var i = 0; i < line.Length; i++)
+        {
+            letter = line[i];
+            isCorrupted = false;
+            
+            switch (letter)
+            {
+                case '(':
+                    stack.Push(letter);
+                    break;
+                case ')':
+                    isCorrupted = stack.SafePeek() != '(';
+                    if (!isCorrupted) stack.Pop();
+                    break;
+            
+                case '[':
+                    stack.Push(letter);
+                    break;
+                case ']':
+                    isCorrupted = stack.SafePeek() != '[';
+                    if (!isCorrupted) stack.Pop();
+                    break;
+            
+                case '{':
+                    stack.Push(letter);
+                    break;
+                case '}':
+                    isCorrupted = stack.SafePeek() != '{';
+                    if (!isCorrupted) stack.Pop();
+                    break;
+            
+                case '<':
+                    stack.Push(letter);
+                    break;
+                case '>':
+                    isCorrupted = stack.SafePeek() != '<';
+                    if (!isCorrupted) stack.Pop();
+                    break;
+            
+                default:
+                    break;
+            }
+
+            if (isCorrupted)
+                break;
+        }
+        
+        if (isCorrupted)
+            continue;
+
+        while (stack.Count > 0)
+        {
+            var l = stack.Pop();
+            answer = answer * 5 + pointsMap[l];
+        }
+        
+        answers.Add(answer);
+        
+    }
+    
+    answers.Sort();
+    var x = answers[answers.Count / 2];
+
+    return x;
 }
 
 public static class Extensions
 {
-    public static void RemoveLast(this List<int> list)
+    public static char SafePop(this Stack<char> stack)
     {
-        if (list.Count == 0)
-            return;
-        
-        list.RemoveAt(list.Count - 1);
+        return stack.Any() ? stack.Pop() : default;
+    }
+    
+    public static char SafePeek(this Stack<char> stack)
+    {
+        return stack.Any() ? stack.Peek() : default;
     }
 }
